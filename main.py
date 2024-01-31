c0049ab27470624d8df89ec8ac3073ae365310cd
VeRsASRomAA <157576722+VeRsASRomAA@users.noreply.github.com>
2024-01-31 14:12:02 +0300

    31.01.2024 на паре с А.С.Морозовым задание №1

---
+++ main.py
@@ -0,0 +1,13 @@
+tseloe =10
+drobnoe = 8.4
+stroka = "No"
+big_netseloe= tseloe + 3.5
+drobnoe -=1
+tseloe/drobnoe
+big_netseloe/drobnoe 
+stroka= "NoNo_YesYesYes"
+
+print(tseloe)
+print(drobnoe)
+print(stroka)
+print(big_netseloe)
